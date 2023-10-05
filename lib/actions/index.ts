44d3b8df3197b55dfbdf840c0a5f452080bc43e9
mccantsquincy <quincymccants@gmail.com>
2023-10-04 22:31:35 -0700

    mongodb setup

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,12 +1,24 @@
 "use server";
 
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
 
     try {
-        const scrapedProduct = await scrapeAmazonProduct(productUrl)
+        const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+        if(!scrapedProduct) return;
+
+        // store or find scraped product data in database
+        try {
+            connectToDB();
+
+        } catch(error: any) {
+            throw new Error(`Failed to connect to DB ${error.message}`);
+        }
+        
     } catch (error: any) {
         throw new Error(`Failed to update product: ${error.message}`)
     }
