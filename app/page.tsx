e033275ac5bfe086a228749074573f9c5a37b0b3
mccantsquincy <quincymccants@gmail.com>
2023-10-06 20:43:06 -0700

    setting up home page ProductCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Searchbar from "@/components/Searchbar";
 import Image from "next/image";
+import { getAllProducts } from "@/lib/actions";
+import ProductCard from "@/components/ProductCard";
 
-export default function Home() {
+const Home = async () => {
+
+  const allProducts = await getAllProducts();
+
   return (
     <>
       <section className="px-6 md:px-20 py-24">
@@ -23,11 +28,16 @@ export default function Home() {
         <h2 className="section-text">Trending</h2>
 
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {["Apple Iphone", "Book", "Sneakers"].map((product) => (
-            <div>{product}</div>
+          {allProducts?.map((product) => (
+            <ProductCard 
+              key={product._id}
+              product={product}
+            />
           ))}
         </div>
       </section>
     </>
   );
 }
+
+export default Home;
---
+++ components/ProductCard.tsx
@@ -0,0 +1,14 @@
+import { Product } from '@/types';
+import React from 'react'
+
+interface Props {
+    product: Product;
+}
+
+const ProductCard = ({ product }: Props) => {
+  return (
+    <div>ProductCard</div>
+  )
+}
+
+export default ProductCard
