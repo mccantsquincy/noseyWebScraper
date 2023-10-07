7064c366a46734777a86232bfbcb0ed16e4585ba
mccantsquincy <quincymccants@gmail.com>
2023-10-06 20:52:25 -0700

    displaying product image and title

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,4 +1,6 @@
 import { Product } from '@/types';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 interface Props {
@@ -7,7 +9,27 @@ interface Props {
 
 const ProductCard = ({ product }: Props) => {
   return (
-    <div>ProductCard</div>
+    <Link href={`/products/${product._id}`} className="product-card">
+      <div className='product-card_img-container'>
+        <Image 
+          src={product.image}
+          alt={product.title}
+          width={200}
+          height={200}
+          className="product-card_img"
+        />
+      </div>
+
+      <div className='flex flex-col gap-3'>
+        <h3 className='product-title'>{product.title}</h3>
+
+        <div className='flex justify-between'>
+            <p className='text-black opacity-50 text-lg capitalize'>
+                {product.category}
+            </p>
+        </div>
+      </div>
+    </Link>
   )
 }
 
