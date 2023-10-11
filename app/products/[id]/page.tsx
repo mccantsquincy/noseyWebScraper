b561971fbf7b8e44a6e1740d8fc6dd7d24a5e2e7
mccantsquincy <quincymccants@gmail.com>
2023-10-10 20:33:08 -0700

    removed unecessary elems. working on productInfo

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,69 +1,88 @@
-import { getProductById } from '@/lib/actions'
-import Image from 'next/image';
-import Link from 'next/link';
-import { redirect } from 'next/navigation';
-import React from 'react'
+import { getProductById } from "@/lib/actions";
+import { formatNumber } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import React from "react";
 
 interface Props {
-  params: { id: string }
+  params: { id: string };
 }
 
-const ProductDetails = async ({ params : { id }  } : Props) => {
-  
+const ProductDetails = async ({ params: { id } }: Props) => {
   const product = await getProductById(id);
 
-  if(!product) redirect('/');
-
-
+  if (!product) redirect("/");
 
   return (
-    <div className='product-container'>
-      <div className='flex gap-28 cl:lex-row flex-col'>
-        <div className='product-image'>
+    <div className="product-container">
+      <div className="flex gap-28 cl:lex-row flex-col">
+        <div className="product-image">
           <Image
-             src={product.image}
-             alt={product.title}
-             width={580}
-             height={400}
-             className="mx-auto"
+            src={product.image}
+            alt={product.title}
+            width={580}
+            height={400}
+            className="mx-auto"
           />
         </div>
 
-        <div className='flex-1 flex flex-col'>
-          <div className='flex justify-between items-start gap-5 flex-wrap pb-6'>
-            <div className='flex flex-col gap-3'>
-              <p className='text-[28px] text-gray-900 font-semibold'>{product.title}</p>
+        <div className="flex-1 flex flex-col">
+          <div className="flex justify-between items-start gap-5 flex-wrap pb-6">
+            <div className="flex flex-col gap-3">
+              <p className="text-[28px] text-gray-900 font-semibold">
+                {product.title}
+              </p>
 
               <Link
-                 href={product.url}
-                 target="_blank"
-                 className='text-base text-black opacity-50'
+                href={product.url}
+                target="_blank"
+                className="text-base text-black opacity-50"
               >
                 Visit Product
               </Link>
             </div>
-
-            <div className='flex items-center gap-3'>
-              <div className='product-hearts rounded-lg'>
-                 <Image 
-                    src="/assets/icons/Heart.svg"
-                    alt="heart"
-                    height={20}
-                    width={20}
-                 />
-
-                 <p className='text-base font-semibold text-[#D46F77]'>{product.reviewsCount}</p>
-              </div>
-              
-              <div className='p-2 bg-white-200 rounded-lg'>
-                {/* Finish product card */}
-              </div>
-            </div>
           </div>
+        </div>
+        <div className="product-info">
+           <div className="flex flex-col gap-2">
+             <p className="text-[34px] text-gray-900 font-bold">
+              {product.currency} {formatNumber(product.currentPrice)}
+             </p>
+             <p className="text-[21px] text-gray-900 opacity-50 line-through">
+              {product.currency} {formatNumber(product.originalPrice)}
+             </p>
+           </div>
+
+           <div className="flex flex-col gap-4">
+             <div className="flex gap-3">
+                <div className="product-stars">
+                  <Image 
+                     src="/assets/icons/Star.svg"
+                     alt="star"
+                     width={20}
+                     height={20}
+                  />
+                  <p className="text-sm font-semibold">{product.stars || 25}</p>
+                </div>
+
+                <div className="product-reviews">
+                     <Image
+                        src="/assets/icons/Comment.svg"
+                        alt="comment"
+                        width={20}
+                        height={20}
+                     />
+                     <p className="text-sm text-gray-900 font-semibold">
+                      {product.reviewsCount || 100} Reviews
+                     </p>
+                </div>
+             </div>
+           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProductDetails;
