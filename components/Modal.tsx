035ae26c72f99094de92cf423bd1e159fb44f794
mccantsquincy <quincymccants@gmail.com>
2023-10-18 23:08:43 -0700

    enabling open and closing of modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,33 +1,39 @@
 "use client";
 
-import { useState } from "react";
-import { Dialog } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 
 const Modal = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => setIsOpen(true);
+
+  const closeModal = () => setIsOpen(false);
 
   return (
     <>
-      <button type="button" className="btn">
+      <button type="button" className="btn" onClick={openModal}>
         Track
       </button>
 
-      <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-        <Dialog.Panel>
-          <Dialog.Title>Deactivate account</Dialog.Title>
-          <Dialog.Description>
-            This will permanently deactivate your account
-          </Dialog.Description>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" onClose={closeModal} className="dialog-container">
+          <Dialog.Panel>
+            <Dialog.Title>Deactivate account</Dialog.Title>
+            <Dialog.Description>
+              This will permanently deactivate your account
+            </Dialog.Description>
 
-          <p>
-            Are you sure you want to deactivate your account? All of your data
-            will be permanently removed. This action cannot be undone.
-          </p>
+            <p>
+              Are you sure you want to deactivate your account? All of your data
+              will be permanently removed. This action cannot be undone.
+            </p>
 
-          <button onClick={() => setIsOpen(false)}>Deactivate</button>
-          <button onClick={() => setIsOpen(false)}>Cancel</button>
-        </Dialog.Panel>
-      </Dialog>
+            <button onClick={() => setIsOpen(false)}>Deactivate</button>
+            <button onClick={() => setIsOpen(false)}>Cancel</button>
+          </Dialog.Panel>
+        </Dialog>
+      </Transition>
     </>
   );
 };
