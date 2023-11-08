a16ece1afeaa22b7a642f95dae757259d3c58559
mccantsquincy <quincymccants@gmail.com>
2023-11-08 11:29:16 -0800

    fixed modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -5,14 +5,16 @@ import { Dialog, Transition } from "@headlessui/react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { addUserEmailToProduct } from "@/lib/actions";
+import Image from "next/image";
+import { Mail, XIcon } from "lucide-react";
 
 interface Props {
   productId: string;
 }
 
-const Modal = ({ productId } : Props) => {
+const Modal = ({ productId }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [email, setEmail] = useState('')
+  const [email, setEmail] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const openModal = () => setIsOpen(true);
@@ -29,7 +31,7 @@ const Modal = ({ productId } : Props) => {
 
     setIsSubmitting(false);
 
-    setEmail('');
+    setEmail("");
 
     closeModal();
   };
@@ -41,36 +43,83 @@ const Modal = ({ productId } : Props) => {
       </button>
 
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog
-          as="div"
-          onClose={closeModal}
-          className="dialog-container flex items-center justify-center"
-        >
-          <Dialog.Panel className="dialog-content">
-            <div>close btn</div>
-            <Dialog.Title className="dialog-head_text">
-              Stay updated with the best market prices straight to your inbox!
-            </Dialog.Title>
-            <Dialog.Description className="my-4">
-              Never miss the best prices again.
-            </Dialog.Description>
+        <Dialog as="div" onClose={closeModal} className="dialog-container">
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterTo="opacity-100"
+              leave="ease-in duration 200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
 
-            <form action="" onSubmit={handleSubmit}>
-              <Input
-                required
-                id="email"
-                value={email}
-                className="text-neutral-900"
-                type="email"
-                placeholder="your-email@gmail.com"
-                onChange={(e) => setEmail(e.target.value)}
-              />
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            />
 
-              <Button type="submit" className="w-full my-4">
-                {isSubmitting ? "Submitting..." : "Track"}
-              </Button>
-            </form>
-          </Dialog.Panel>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="dialog-content">
+                <div className="flex flex-col">
+                  <div className="flex justify-end">
+                    <XIcon 
+                      onClick={closeModal}
+                      className="hover:cursor-pointer"
+                    />
+                  </div>
+
+                  <h4 className="dialog-head_text">
+                    Stay updated on the best prices for your favorite amazon
+                    products
+                  </h4>
+
+                  <p className="text-sm text-gray-600 mt-2">
+                    Never miss a bargain again with our timely alerts!
+                  </p>
+                </div>
+
+                <form className="flex flex-col mt-5" onSubmit={handleSubmit}>
+                  <label
+                    htmlFor="email"
+                    className="text-sm font-medium text-gray-700"
+                  >
+                    Email address
+                  </label>
+                  <div className="dialog-input_container">
+                    <Mail />
+
+                    <input
+                      required
+                      type="email"
+                      id="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      placeholder="Enter your email address"
+                      className="outline-none"
+                    />
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="dialog-btn"
+                  >
+                    {isSubmitting ? "Submitting..." : "Track"}
+                  </button>
+                </form>
+              </div>
+            </Transition.Child>
+          </div>
         </Dialog>
       </Transition>
     </>
